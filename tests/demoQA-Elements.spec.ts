1c2bc8492aafe458cf52cf41ce7401007dc6df61
Luis Morla <luismorla@MacBook-Air-de-Luis.local>
2026-03-09 08:08:21 -0400

    add fixtures

--- tests/demoQA-Elements.spec.ts
+++ tests/demoQA-Elements.spec.ts
@@ -1,26 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';    
-import {faker} from '@faker-js/faker'
 import { argosScreenshot } from "@argos-ci/playwright";
+import { test } from '../test-options'
 
 test.describe('Elements', async () => {
     test.beforeEach(async({page}) =>{
-    await page.goto('https://demoqa.com/');
     await argosScreenshot(page, "homepage");
   })
 
-  test('Validate Elements', async({page})=>{
-    const randomFullName = faker.person.fullName()
-    const nameClean = faker.person.firstName().replace(/\s/g, '')
-    const randomNumber = faker.number.int(100)
-    const randomEmail = `${nameClean}${randomNumber}@test.com`
-    const randomCity = faker.location.city()
-    const randomAddress = faker.location.streetAddress()
-    const randomZipCode = faker.location.zipCode()
-    const randomCurrentAddress = `${randomAddress}${randomCity}${randomZipCode}`
-    const randomPermanentAddress = `${randomAddress}${randomCity}${randomZipCode}`
-    const pm = new PageManager(page)
-    await pm.onElements().elementsButton()
-    await pm.onElements().textBoxes(randomFullName, randomEmail, randomCurrentAddress, randomPermanentAddress)
+  test('Validate Elements', async({pageManager, fakerData})=>{
+    await pageManager.onElements().elementsButton()
+    await pageManager.onElements().textBoxes(fakerData.user, fakerData.email, fakerData.cAddress, fakerData.pAddress)
   })
 });
